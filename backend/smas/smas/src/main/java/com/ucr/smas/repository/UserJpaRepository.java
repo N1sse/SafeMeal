9545b00163c8ddb2e6b63bc47c2db1924ceee8c1
N1sse <ramirezdenisse01@gmail.com>
2026-06-01 16:42:45 -0600

    Empezando el modulo del user con el entity, repository and service

--- backend/smas/smas/src/main/java/com/ucr/smas/repository/UserJpaRepository.java
+++ backend/smas/smas/src/main/java/com/ucr/smas/repository/UserJpaRepository.java
@@ -1,4 +1,10 @@
 package com.ucr.smas.repository;
 
-public interface UserJpaRepository {
+import com.ucr.smas.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserJpaRepository extends JpaRepository<User, Integer> {
+    User getByEmail(String email);
 }
--- backend/smas/smas/src/main/java/com/ucr/smas/service/UserService.java
+++ backend/smas/smas/src/main/java/com/ucr/smas/service/UserService.java
@@ -1,4 +1,30 @@
 package com.ucr.smas.service;
 
+import com.ucr.smas.model.User;
+import com.ucr.smas.repository.UserJpaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class UserService {
+
+    @Autowired
+    private UserJpaRepository userRepository;
+
+    //Encontrar todos los usuarios
+    public List<User> findAll(){
+        return userRepository.findAll();
+    }
+
+    //Encontrar el usuario por el Id
+
+    //Guardar el usuario
+
+    //Actualizar el usuario
+
+    //eliminar el usuario
+
+
 }
