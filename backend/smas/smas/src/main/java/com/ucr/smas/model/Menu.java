050d01fcd48a7a7abddbfcb1a8ba3486199e5a66
BarbedFan077992 <kendalltutuyo.perezcruz@gmail.com>
2026-06-04 12:59:05 -0600

    Ya agregué el Menu, con sus atributos, getters, setters y sus respectivas decoraciones

---
+++ backend/smas/smas/src/main/java/com/ucr/smas/model/Menu.java
@@ -0,0 +1,70 @@
+package com.ucr.smas.model;
+
+import jakarta.persistence.*;
+import org.springframework.data.annotation.Id;
+
+@Entity
+@Table(name="tb-menu")
+public class Menu {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String menuName;
+
+    @JoinColumn(name="user_id")
+    private User user;
+   /* @JoinColumn(name="padecimientos_id")
+    private Padecimientos padecimientos;
+    */
+    private String food;
+    private String description;
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getMenuName() {
+        return menuName;
+    }
+
+    public void setMenuName(String menuName) {
+        this.menuName = menuName;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+/*
+    public Padecimientos getPadecimientos() {
+        return padecimientos;
+    }
+
+    public void setPadecimientosId(Padecimientos padecimientos) {
+        this.padecimientos = padecimientos;
+    }
+*/
+    public String getFood() {
+        return food;
+    }
+
+    public void setFood(String food) {
+        this.food = food;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
