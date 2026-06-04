ff1c39b86942579bf2f53f11c5cccaeeacec5a30
N1sse <ramirezdenisse01@gmail.com>
2026-06-04 15:38:08 -0600

    Avancé en el módulo de usuarios: DTO con validaciones, Repository con métodos de consulta por correo y Service con lógica para registro, búsqueda, actualización y eliminación de usuarios.

--- backend/smas/smas/src/main/java/com/ucr/smas/model/dto/UserDTO.java
+++ backend/smas/smas/src/main/java/com/ucr/smas/model/dto/UserDTO.java
@@ -1,4 +1,84 @@
 package com.ucr.smas.model.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.*;
+
+import java.time.LocalDate;
+
 public class UserDTO {
-}
+
+    @NotBlank(message = "El espacio del nombre del usuario no puede ir vacío, por favor ingrese un nombre")
+    private String name;
+
+    @NotBlank(message = "El espacio del género no puede ir vacío, por favor ingrese su género")
+    private String gender;
+
+    @JsonFormat(pattern = "dd-MM-yyyy")
+    @NotNull(message = "Por favor ingrese la fecha de su cumpleaños")
+    private LocalDate dateBirth;
+
+    @Email(message = "Por favor, ingrese un correo electrónico válido.")
+    @NotBlank(message = "El espacio del correo no puede quedar vacio")
+    private String email;
+
+    @NotBlank(message = "La contraseña es obligatoria.")
+    @Size(min = 8, max = 64, message = "La contraseña debe tener entre 8 y 64 caracteres.")
+    @Pattern(
+            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[.,+?!*#$@%&()_\\-={}\\[\\]:;<>/]).+$",
+            message = "La contraseña debe tener un mínimo de 8caracteres; otras consideraciones es que la contraseña debe de tener al menos una minúscula, una mayúscula, un número, un carácter especial."
+    )
+    private String password;
+
+    //metodos contructores
+    public UserDTO() {
+    }
+
+    public UserDTO(String name, String gender, LocalDate dateBirth, String email, String password) {
+        this.name = name;
+        this.gender = gender;
+        this.dateBirth = dateBirth;
+        this.email = email;
+        this.password = password;
+    }
+
+    //sets y gets
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public LocalDate getDateBirth() {
+        return dateBirth;
+    }
+
+    public void setDateBirth(LocalDate dateBirth) {
+        this.dateBirth = dateBirth;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}//fin UserDTO
--- backend/smas/smas/src/main/java/com/ucr/smas/service/UserService.java
+++ backend/smas/smas/src/main/java/com/ucr/smas/service/UserService.java
@@ -1,11 +1,14 @@
 package com.ucr.smas.service;
 
 import com.ucr.smas.model.User;
+import com.ucr.smas.model.dto.UpdatePasswordDTO;
+import com.ucr.smas.model.dto.UserDTO;
 import com.ucr.smas.repository.UserJpaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -13,18 +16,110 @@ public class UserService {
     @Autowired
     private UserJpaRepository userRepository;
 
-    //Encontrar todos los usuarios
+    //---Encontrar todos los usuarios
     public List<User> findAll(){
         return userRepository.findAll();
     }
 
-    //Encontrar el usuario por el Id
+    //---Encontrar el usuario por el Id
+    public User getById(Integer id){
 
-    //Guardar el usuario
+        Optional<User> userExists= userRepository.findById(id);
+        if(userExists.isPresent()){
+            User userTemp = userExists.get();
+            return userTemp;
+        }
+        return  null;
+    }
 
-    //Actualizar el usuario
+    //---Guardar el usuario
+    public User add(UserDTO user){
+        if (userRepository.existsByEmail((user.getEmail()))){
+            return null;
+        }else{
+            if (user.getName() == null || user.getGender() == null || user.getDateBirth() == null || user.getEmail() == null || user.getPassword() == null){
+                return  null;
+            }
+        }
+        User userTemp=new User();
+        userTemp.setName(user.getName());
+        userTemp.setGender(user.getGender());
+        userTemp.setDateBirth(user.getDateBirth());
+        userTemp.setEmail(user.getEmail());
+        userTemp.setPassword(user.getPassword());
+        return userRepository.save(userTemp);
+    }//fin add
 
-    //eliminar el usuario
+    //---Actualizar el usuario
+    public User updateUser(Integer id, UserDTO user){
+        Optional<User> userExists = userRepository.findById(id);
 
+        if (userExists.isPresent()){
+            User userTemp = userExists.get();
+
+            //Para poder verificar que el espacio no este en null y actulizar el nombre
+            if (user.getName() != null){
+                userTemp.setName(user.getName());
+            }
+
+            //Para poder verificar que el espacio no este en null y actulizar el género
+            if (user.getGender() != null){
+                userTemp.setGender(user.getGender());
+            }
+
+            //Para poder verificar que el espacio no este en null
+            // y actulizar la fecha en la que nació el usuario
+            if (user.getDateBirth()!=null){
+                userTemp.setDateBirth(user.getDateBirth());
+            }
+
+            //Para poder verificar que el espacio no este en null y actulizar el email
+            if (user.getEmail()!=null){
+                userTemp.setEmail(user.getEmail());
+            }
+
+            //Para poder verificar que el espacio no este en null y actulizar la contraseña
+            if (user.getPassword()!=null){
+                userTemp.setPassword(user.getPassword());
+            }
+
+            //Para que se guarde
+            return userRepository.save(userTemp);
+        }
+        return null;
+    }//fin updateUser
+
+    //---Eliminar el usuario
+    public User delete(Integer id){
+        Optional<User> existsUser=userRepository.findById(id);
+
+        if (existsUser.isPresent()){
+            userRepository.deleteById(id);
+            return existsUser.get();
+        }else {
+            return null;
+        }
+    }
+
+    //---Para cambiar la contraseña
+    public User updatePassword(UpdatePasswordDTO request){
+        if (request.getEmail()==null||request.getPassword()==null){
+            return  null;
+        }
+
+        Optional<User> userExists = userRepository.findByEmail(request.getEmail());
+        if (userExists.isPresent()){
+            User userTemp = userExists.get();
+            userTemp.setPassword(request.getPassword());
+            return userRepository.save(userTemp);
+        }else {
+            return null;
+        }
+    }
+
+    //---Obtener por email
+    public  User getUserByEmail(String email){
+        return userRepository.getByEmail(email);
+    }
 
 }
